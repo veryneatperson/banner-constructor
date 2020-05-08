150abc103d1dbef4144fa6c778ae68449df971a6
veryneatperson <62647942+veryneatperson@users.noreply.github.com>
2020-05-08 22:10:48 +0300

    Added <FormField>

---
+++ src/components/FormField/FormField.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import cx from 'classnames';
+
+import styles from './FormField.module.scss';
+
+const FormField = ({ field, onChange }) => {
+  const { label, errorMsg, touched, valid, validatorFn, defaultValue, ...attributes } = field;
+
+  const constructField = () => {
+    const classes = cx(styles.Field, { [styles.valid]: valid, [styles.invalid]: touched && !valid });
+
+    if (attributes.type === 'select') {
+      return (
+        <select {...attributes} className={classes} onChange={onChange}>
+          <option value={defaultValue} disabled>
+            -- select an option --
+          </option>
+          {field.options.map((el) => (
+            <option key={el.name} name={el.name}>
+              {el.label}
+            </option>
+          ))}
+        </select>
+      );
+    } else {
+      return <input className={classes} onChange={onChange} {...attributes} />;
+    }
+  };
+
+  return (
+    <div className={styles.FormField}>
+      <label className={styles.Label} htmlFor="">
+        {label}
+      </label>
+      {constructField()}
+      {touched && !valid ? <span className={styles.ErrorMsg}>{errorMsg}</span> : null}
+    </div>
+  );
+};
+
+export default React.memo(FormField);
